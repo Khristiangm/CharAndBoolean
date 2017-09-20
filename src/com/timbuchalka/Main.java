42ea722853617251ebca53b627f36aa16d024a10
Khristian Guimaraes Marchioro (BFSI - Brazil) <KH389659@wipro.com>
2017-09-20 13:39:15 -0300

    first commit

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,14 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char myChar = '\u00AE';
+        System.out.println("Unicode output was: " + myChar );
+
+    boolean myBooLean = false;
+    boolean isMale = true;
+
+
+    }
+}
